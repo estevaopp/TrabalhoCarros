02134497d9d8a9cf72358369531dbc70cf124327
estevao.pereira <estevaopp05@gmail.com>
2023-10-14 10:00:33 -0300

    ajduatando caminho

--- WevertonTrabalho/src/reports/relatorios.py
+++ WevertonTrabalho/src/reports/relatorios.py
@@ -3,19 +3,19 @@ from conexion.oracle_queries import OracleQueries
 class Relatorio:
     def __init__(self):
         # Abre o arquivo com a consulta e associa a um atributo da classe
-        with open("/home/labdatabase/GitHub/OracleTrabalhoHoward/src/sql/relatorio_itensCarrinhos.sql") as f:
+        with open("/home/labdatabase/GitHub/TrabalhoCarros/WevertonTrabalho/src/sql/relatorio_itensCarrinhos.sql") as f:
             self.query_relatorio_itensCarrinhos = f.read()
 
         # Abre o arquivo com a consulta e associa a um atributo da classe
-        with open("/home/labdatabase/GitHub/OracleTrabalhoHoward/src/sql/relatorio_itensCarrinhos_por_carrinho.sql") as f:
+        with open("/home/labdatabase/GitHub/TrabalhoCarros/WevertonTrabalho/src/sql/relatorio_itensCarrinhos_por_carrinho.sql") as f:
             self.query_relatorio_itensCarrinhos_por_carrinho = f.read()
 
         # Abre o arquivo com a consulta e associa a um atributo da classe
-        with open("/home/labdatabase/GitHub/OracleTrabalhoHoward/src/sql/relatorio_carrinhos.sql") as f:
+        with open("/home/labdatabase/GitHub/TrabalhoCarros/WevertonTrabalho/src/sql/relatorio_carrinhos.sql") as f:
             self.query_relatorio_carrinhos = f.read()
 
         # Abre o arquivo com a consulta e associa a um atributo da classe
-        with open("/home/labdatabase/GitHub/OracleTrabalhoHoward/src/sql/relatorio_produtos.sql") as f:
+        with open("/home/labdatabase/GitHub/TrabalhoCarros/WevertonTrabalho/src/sql/relatorio_produtos.sql") as f:
             self.query_relatorio_produtos = f.read()
 
     def get_relatorio_itensCarrinhos(self):
